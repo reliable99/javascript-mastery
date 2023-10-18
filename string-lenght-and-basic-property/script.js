b5ea22eedfe933d0d2986e604e251d7d1c93fe62
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-18 12:33:06 +0100

    first commit

---
+++ string-lenght-and-basic-property/script.js
@@ -0,0 +1,18 @@
+console.log("string length");
+
+// One thing that we often want to know when it comes to strings is its lenght. You might think that we need to do some complex stuff to come to that value. Like loop through all the characters, count them and then display the value. It's so much simpler than that!
+
+const name = "ade";
+console.log(name.length);
+
+// Another thing we might want to do often is get the element at a certain position of the string. We can do it really easily as well.
+
+// This is how we would get the first letter of a string:
+
+const name1 = "adekilekun";
+const firstLetter = name1[0];
+const secongLetter = name1[5];
+console.log(firstLetter, secongLetter);
+// console.log([name.length - 1]);
+
+// ... and this is how we would get the last letter:
