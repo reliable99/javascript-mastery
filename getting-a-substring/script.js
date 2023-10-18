28debc1d0c4139f73b2857c9c5736a1f60731527
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-18 23:58:06 +0100

    second commit

---
+++ getting-a-substring/script.js
@@ -0,0 +1,9 @@
+console.log("getting a substring");
+
+// The best method for getting a substring of a string is slice() method
+
+// Returns the part of the string from start to end
+
+const slice = "bread";
+const sliceFro = slice.slice(1, 3);
+console.log(sliceFro);
