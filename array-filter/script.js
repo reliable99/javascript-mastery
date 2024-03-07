eb83dfe0167aa36026b7ac4e5b1b3fa824b1d72e
reliable99 <abdulmuhminsodiq@gmail.com>
2024-03-07 16:10:22 +0100

    sd

--- array-filter/script.js
+++ array-filter/script.js
@@ -1,7 +1,32 @@
 console.log("arrray filter");
 
+
+// filetr method does as the name implies it filters certain element from an array
 const numbers = [-10, 0, -2, 15, -36, 25];
 const positiveNumbers = numbers.filter((number) => {
   return number >= 0;
 });
 console.log(positiveNumbers);
+
+
+// Second Example
+
+// A start up wants to reward the employees with 7 or more hours of overtime
+var employeesData = [
+	{
+    	name: "Sebastian",
+    	overtime: 5
+	},
+	{
+    	name: "Cardi Vee",
+    	overtime: 10
+	},
+	{
+    	name: "George Lopez",
+    	overtime: 12
+	}
+];
+
+const employeesToReward = employeesData.filter((employee) => employee.overtime >= 7);
+console.log(employeesToReward)
+
